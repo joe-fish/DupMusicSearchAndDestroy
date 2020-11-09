61551c186b97e7d013a4d710ef4a6efac11d6a39
loserjoe <joefischer1@yahoo.com>
2020-11-09 16:08:44 -0500

    Added delete capability and fixed status panel

--- src/main/java/gui/AppWindow.java
+++ src/main/java/gui/AppWindow.java
@@ -10,13 +10,17 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+
 import javax.swing.JToolBar;
+import javax.swing.SwingConstants;
 
 import searcher.MusicItem;
 import searcher.Searcher;
 
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.AbstractAction;
 
 import java.io.File;
@@ -32,13 +36,16 @@ import javax.swing.JFileChooser;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class AppWindow {
 
-	private JFrame frame;
+	private static JFrame frame;
 	private final JPanel panel = new JPanel();
 	private File searchFolder = null;
-
+	private File dupeListSelected = null;
+	private static JLabel status;
 
 	/**
 	 * Launch the application.
@@ -48,6 +55,8 @@ public class AppWindow {
 			try {
 				AppWindow window = new AppWindow();
 				window.frame.setVisible(true);
+				//displayStatus("Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing ");
+
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -56,6 +65,7 @@ public class AppWindow {
 
 	/**
 	 * Create the application.
+	 * 
 	 * @wbp.parser.entryPoint
 	 */
 	public AppWindow() {
@@ -70,99 +80,132 @@ public class AppWindow {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
-		
+
 		JMenuBar topMenuBar = new JMenuBar();
 		frame.setJMenuBar(topMenuBar);
-		
+
 		JMenu fileMenu = new JMenu("File");
 		topMenuBar.add(fileMenu);
-		
+
 		JMenuItem mntmNewMenuItem = new JMenuItem("Select Search Folder");
 		fileMenu.add(mntmNewMenuItem);
-		
-		JPanel controlArea = new JPanel(new GridLayout(2,0));
+
+		JPanel controlArea = new JPanel(new GridLayout(2, 0));
 		JToolBar toolBar = new JToolBar();
 		toolBar.setFloatable(false);
-		
+
 		JLabel searchFolderName = new JLabel("Not Selected");
 		controlArea.add(toolBar);
 		controlArea.add(searchFolderName);
 		frame.getContentPane().add(controlArea, BorderLayout.NORTH);
-		
-		//Build Status line		
-		JPanel statusPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+		// Build Status line
+		JPanel statusPanel = new JPanel(new GridBagLayout());
+		GridBagConstraints statusConstraint = new GridBagConstraints();
+		statusConstraint.fill = GridBagConstraints.HORIZONTAL;
+		statusConstraint.gridheight=1;
+		statusConstraint.gridx = 0;
+		statusConstraint.gridy = 0;
 		JLabel statusLabel = new JLabel("Status: ");
-		statusPanel.add(statusLabel);
-		
-		JLabel status = new JLabel("");
-		statusPanel.add(status);
-		
-		
+		statusPanel.add(statusLabel,statusConstraint );
+		statusConstraint.gridx = 1;
+		statusConstraint.gridy = 0;
+		statusConstraint.weightx= 0.2;
+		statusConstraint.weighty= 0.2;
+		status = new JLabel("");
+		statusPanel.add(status, statusConstraint);
 		frame.getContentPane().add(statusPanel, BorderLayout.SOUTH);
-		
+
 		JButton selectSearchRootButton = new JButton("Select Search Root");
 		toolBar.add(selectSearchRootButton);
-		
+
 		JButton searchButton = new JButton("Search");
 		toolBar.add(searchButton);
-		
+
 		JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
 		frame.getContentPane().add(splitPane, BorderLayout.CENTER);
-		
+
 		JScrollPane searchedPane = new JScrollPane();
 		splitPane.setTopComponent(searchedPane);
-		
+
 		JScrollPane dupePane = new JScrollPane();
 		splitPane.setBottomComponent(dupePane);
 		dupePane.setViewportView(panel);
-		
+
 		JList<MusicItem> searchList = new JList<MusicItem>();
 		searchedPane.setViewportView(searchList);
-	
+
 		JList<File> dupeList = new JList<File>();
 		searchedPane.setViewportView(dupeList);
-		
+
 		selectSearchRootButton.addActionListener(e -> {
-		    JFileChooser fileChooser = new JFileChooser();
-		    fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-		    int option = fileChooser.showOpenDialog(frame);
-		    if(option == JFileChooser.APPROVE_OPTION){
-		       searchFolder = fileChooser.getSelectedFile();
-		       searchFolderName.setText("Folder Selected: " + searchFolder.getPath());
-		    }else{
-		    	searchFolderName.setText("Select Search Root canceled");
-		    }
-		 });
-		
+			JFileChooser fileChooser = new JFileChooser();
+			fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			int option = fileChooser.showOpenDialog(frame);
+			if (option == JFileChooser.APPROVE_OPTION) {
+				searchFolder = fileChooser.getSelectedFile();
+				searchFolderName.setText("Folder Selected: " + searchFolder.getPath());
+			} else {
+				searchFolderName.setText("Select Search Root canceled");
+			}
+		});
+
 		mntmNewMenuItem.addActionListener(e -> {
 			JFileChooser fileChooser = new JFileChooser();
 			fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 			int option = fileChooser.showOpenDialog(frame);
-			if(option == JFileChooser.APPROVE_OPTION){
+			if (option == JFileChooser.APPROVE_OPTION) {
 				searchFolder = fileChooser.getSelectedFile();
 				searchFolderName.setText("Folder Selected: " + searchFolder.getPath());
-			}else{
+			} else {
 				searchFolderName.setText("Select Search Root canceled");
 			}
 		});
-		
+
 		searchButton.addActionListener(e -> {
 			Searcher searcher = new Searcher();
-			if(searchFolder != null) {
+			if (searchFolder != null) {
 				searcher.doSearch(searchFolder);
 				DefaultListModel model = searcher.getHasDupesModel();
 				searchList.setModel(searcher.getHasDupesModel());
 				searchedPane.setViewportView(searchList);
-				status.setText(Integer.toString(model.getSize())+" Duplicates found");
+				status.setText(Integer.toString(model.getSize()) + " Duplicates found");
 			}
 		});
-		
-		
+
 		searchList.addListSelectionListener(e -> {
 			MusicItem selected = searchList.getSelectedValue();
-			DefaultListModel<File> dupes = selected.getDuplicatesModel();
-			dupeList.setModel(dupes);
-			dupePane.setViewportView(dupeList);
+			if (selected != null) {
+				DefaultListModel<File> dupes = selected.getDuplicatesModel();
+				dupeList.setModel(dupes);
+				dupePane.setViewportView(dupeList);
+			}
+		});
+
+		dupeList.addListSelectionListener(e -> {
+			dupeListSelected = dupeList.getSelectedValue();
+			if (dupeListSelected != null) {
+				status.setText("Selected: " + dupeListSelected.getPath());
+
+			}
+		});
+
+		dupeList.addKeyListener(new KeyAdapter() {
+			
+			public void keyPressed(KeyEvent e) {
+				switch (e.getKeyCode()) {
+				case KeyEvent.VK_DELETE:
+				case KeyEvent.VK_BACK_SPACE:
+					int a = JOptionPane.showConfirmDialog(frame, "Do you want to delete: "+dupeListSelected.getName(), "Confirm", JOptionPane.YES_NO_OPTION);
+					if (a == JOptionPane.YES_OPTION) {
+						dupeListSelected.delete();
+						status.setText( "Deleted: "+dupeListSelected.getPath());
+					} else {
+						//do nothing
+					}			
+					break;
+				}
+			}
 		});
 	}
 }
