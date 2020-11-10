056e453a0f6dd6135a6f478b5f4a63e8ed9fc783
loserjoe <joefischer1@yahoo.com>
2020-11-10 11:31:02 -0500

    Fixed help when building with maven

--- src/main/java/gui/HelpDialog.java
+++ src/main/java/gui/HelpDialog.java
@@ -2,49 +2,72 @@ package gui;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+
+import org.apache.commons.io.IOUtils;
+
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
 /**
  * Displays help information for the application
+ * 
  * @author joefischer
  *
  */
 public class HelpDialog {
-	
+
 	private JFrame parent = null;
-	private String message = "";
-	private String helpFileName = "helpFile.txt";
+//	private String message = "";
+	private String helpFileName = "HelpFile.txt";
 
 	public HelpDialog(JFrame frame) {
 		parent = frame;
-		
+
 	}
-	
+
 	public void showHelp() {
-		String message = readHelpFile();
+		String message = readHelpFileS();
 		JOptionPane.showMessageDialog(parent, message, "Application Help", JOptionPane.PLAIN_MESSAGE);
 	}
-	
-	
-	
+
 	/**
 	 * Reads the help text in from the help file
+	 * 
 	 * @return help file contents as a string
 	 */
-	private String readHelpFile(){
+	private String readHelpFile() {
 		String fileName = helpFileName;
-        ClassLoader classLoader = getClass().getClassLoader();
-        String content = "";
-        File file = new File(classLoader.getResource(fileName).getFile());
-        try {
+		ClassLoader classLoader = getClass().getClassLoader();
+		String content = "";
+		File file = new File(classLoader.getResource(fileName).getFile());
+		try {
 			content = new String(Files.readAllBytes(file.toPath()));
-			//System.out.println("Help file found. File =  "+file.getName());
+			// System.out.println("Help file found. File = "+file.getName());
 		} catch (IOException e) {
 			System.out.println("Error reading help file. ");
 			e.printStackTrace();
 		}
-        return content;
+		return content;
+	}
+
+	/**
+	 * Reads the help text in from the help file in project resources
+	 * 
+	 * @return help file contents as a string
+	 */
+	private String readHelpFileS() {
+		String fileName = helpFileName;
+		ClassLoader classLoader = getClass().getClassLoader();
+		String content = "";
+		try (InputStream inputStream = classLoader.getResourceAsStream(fileName)) {
+			content = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+		} catch (IOException e) {
+			System.out.println("Error reading help file. ");
+			e.printStackTrace();
+		}
+		return content;
 	}
 }
